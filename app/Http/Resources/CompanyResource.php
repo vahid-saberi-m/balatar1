9afa845fd10cbf6d4e82198f74b3b2755bca3cb3
vahid-saberi-m <33976610+vahid-saberi-m@users.noreply.github.com>
2018-09-26 12:40:59 +0330

    added id field to company resource

--- app/Http/Resources/CompanyResource.php
+++ app/Http/Resources/CompanyResource.php
@@ -16,6 +16,7 @@ class CompanyResource extends JsonResource
     public function toArray($request)
     {
         return[
+            'id'=>$this->id,
             'name'=>$this->name,
             'company_size'=>$this->company_size,
             'slogan'=>$this->slogan,
