12d6c28925a5afb9cddce5b8c5ee33d829183dc5
vahid-saberi-m <33976610+vahid-saberi-m@users.noreply.github.com>
2018-09-29 12:12:57 +0330

    testing

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * Class UserResource
@@ -28,7 +29,7 @@ class UserResource extends JsonResource
             'password'=>$this->password,
             'role'=>$this->role,
             'position'=>$this->position,
-            'image'=>$this->image,
+            'image'=>Storage::url($this->image),
             'is_approved'=>$this->is_approved,
             'company_is_live'=> $user->company()->exists() && $user->company->is_live
         ];
