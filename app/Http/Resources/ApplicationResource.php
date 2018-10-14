4f78d9b9a18962196324c67e1cd0b7f13a98647c
vahid-saberi-m <33976610+vahid-saberi-m@users.noreply.github.com>
2018-10-14 10:31:55 +0330

    application resource now returns application candidate cv too

--- app/Http/Resources/ApplicationResource.php
+++ app/Http/Resources/ApplicationResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\CandidateCv;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ApplicationResource extends JsonResource
@@ -20,6 +21,8 @@ class ApplicationResource extends JsonResource
             'name'=>$this->name,
             'position'=>$this->position,
             'company'=>$this->company,
+            'cv_folder_id'=>$this->cv_folder_id,
+            'cv'=>CandidateCv::query()->find($this->candidate_cv)
         ];
     }
 }
