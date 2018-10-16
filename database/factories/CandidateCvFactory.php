88ca1bfd4ccc7f70aa4bcd386ede7f5573a788b7
vahid-saberi-m <33976610+vahid-saberi-m@users.noreply.github.com>
2018-10-16 09:07:39 +0330

    changed the candidate Factory to save avatar cv address right

--- database/factories/CandidateCvFactory.php
+++ database/factories/CandidateCvFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(\App\Models\CandidateCv::class, function (Faker $faker) {
     return [
         'candidate_id'=> $faker->randomElement(App\Models\Candidate::all()->pluck('id')->ToArray()),
-        'cv'=> '/public/candidate/cv/avatar.pdf',
+        'cv'=> '/candidate/cvs/avatar.pdf',
         'file_name'=>'some file.pdf'
     ];
 });
