05fecebbeb2eb8d1629d7667285876adaeb62104
vahid <saberi1365@gmail.com>
2019-10-21 15:03:20 +0330

    y

--- app/Http/Middleware/CorsHandler.php
+++ app/Http/Middleware/CorsHandler.php
@@ -18,7 +18,7 @@ class CorsHandler
         header("Access-Control-Allow-Origin: *");
         //ALLOW OPTIONS METHOD
         $headers = [
-//            'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
+            'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
             'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
         ];
         if ($request->getMethod() == "OPTIONS"){
