78c09a921223f864497f1b88c6449616b998bb3a
vahid <saberi1365@gmail.com>
2019-10-21 14:59:11 +0330

    y

--- app/Http/Middleware/CorsHandler.php
+++ app/Http/Middleware/CorsHandler.php
@@ -19,7 +19,7 @@ class CorsHandler
         //ALLOW OPTIONS METHOD
         $headers = [
             'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+//            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
         ];
         if ($request->getMethod() == "OPTIONS"){
             //The client-side application can set only headers allowed in Access-Control-Allow-Headers
