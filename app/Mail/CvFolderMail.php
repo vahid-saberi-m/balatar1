df9127ced0c3343ab991f9d864dc6ae1d217cb56
vahid-saberi-m <33976610+vahid-saberi-m@users.noreply.github.com>
2019-10-26 22:21:47 +0330

    cv folder application email naw gets sent

--- app/Mail/CvFolderMail.php
+++ app/Mail/CvFolderMail.php
@@ -2,6 +2,8 @@
 
 namespace App\Mail;
 
+use App\Models\Application;
+use App\Models\CvFolder;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
@@ -16,9 +18,10 @@ class CvFolderMail extends Mailable
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(CvFolder $cvFolder, Application $application)
     {
-        //
+        $this->cvFolder = $cvFolder;
+        $this->application = $application;
     }
 
     /**
@@ -28,6 +31,12 @@ class CvFolderMail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
-    }
+        $content= str_replace(['{{name}}','{{company}}'], [$this->application->candidate->name,$this->cvFolder->company->name], $this->cvFolder->email_template);
+        return $this->from('no-reply@bala-tar.com', $this->cvFolder->jobPost->user->name)
+            ->subject('آخرین وضعیت درخواست همکاری')
+            ->html($content)
+            ->with([
+                'company'=> $this->cvFolder->company->name,
+                'name' => $this->application->candidate->name,
+            ]);    }
 }
--- app/Repositories/CvFolderRepository.php
+++ app/Repositories/CvFolderRepository.php
@@ -5,6 +5,7 @@
  * Date: 9/7/2018
  * Time: 6:42 AM
  */
+
 namespace App\Repositories;
 
 use App\Models\Company;
@@ -18,32 +19,46 @@ class CvFolderRepository
 {
     public function CreateJobPostCvFolders(JobPost $jobPost)
     {
-        $cvFolderNames=['مردود','صف انتظار','قابل تامل','دعوت به مصاحبه'];
-        foreach ($cvFolderNames as $cvFolderName){
-            CvFolder::create([
-                'name'=>$cvFolderName,
-                'user_id'=>$jobPost->user_id,
-                'job_post_id'=>$jobPost->id,
-                'company_id'=>$jobPost->company_id
-            ]);
+        $cvFolderNames = ['مردود', 'صف انتظار', 'قابل تامل', 'دعوت به مصاحبه'];
+        foreach ($cvFolderNames as $cvFolderName) {
+            if ($cvFolderName!= 'مردود') {
+
+                CvFolder::create([
+                    'name' => $cvFolderName,
+                    'user_id' => $jobPost->user_id,
+                    'job_post_id' => $jobPost->id,
+                    'company_id' => $jobPost->company_id
+                ]);
+            }
+            if ($cvFolderName === 'مردود') {
+
+                CvFolder::create([
+                    'name' => $cvFolderName,
+                    'user_id' => $jobPost->user_id,
+                    'job_post_id' => $jobPost->id,
+                    'company_id' => $jobPost->company_id,
+                    'email_template'=> ' عزیز. ضمن تشکر از ابراز تمایل شما برای همکاری با شرکت{{company}} در سمت شغلی {{job-title}} به اطلاع می رساند متاسفانه در حال حاضر امکان همکاری با شما دوست عزیز برای ما فراهم نمی باشد. با آرزوی بهترین ها  {{name}}'                ]);
+            }
         }
         return response()->json($jobPost);
     }
 
-    public function store(Request $request,JobPost $jobPost){
-      $cvFolder=  CvFolder::query()->create([
-            'name'=>$request->name,
-            'user_id'=>$jobPost->user_id,
-            'job_post_id'=>$jobPost->id,
-            'company_id'=>$jobPost->company_id
+    public function store(Request $request, JobPost $jobPost)
+    {
+        $cvFolder = CvFolder::query()->create([
+            'name' => $request->name,
+            'user_id' => $jobPost->user_id,
+            'job_post_id' => $jobPost->id,
+            'company_id' => $jobPost->company_id
         ]);
-      return $cvFolder->name;
-    }
-    public function updateEmailTemplate(Request $request,CvFolder $cvFolder){
-      $cvFolder->update(['email_template'=>$request->email_template]);
-      return $cvFolder->email_template;
+        return $cvFolder->name;
     }
 
+    public function updateEmailTemplate(Request $request, CvFolder $cvFolder)
+    {
+        $cvFolder->update(['email_template' => $request->email_template]);
+        return $cvFolder->email_template;
+    }
 
 
 }
