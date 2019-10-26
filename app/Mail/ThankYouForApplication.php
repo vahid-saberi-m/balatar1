1496bb07c1277cefef9d49bc207d5316cadfe469
vahid <saberi1365@gmail.com>
2019-10-26 09:26:06 +0330

    ok

--- app/Mail/ThankYouForApplication.php
+++ app/Mail/ThankYouForApplication.php
@@ -1,18 +1,15 @@
 <?php
-
 namespace App\Mail;
-
+use App\Models\CvFolder;
 use App\Models\JobPost;
 use Illuminate\Http\Request;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
-
 class ThankYouForApplication extends Mailable
 {
     use Queueable, SerializesModels;
-
     /**
      * Create a new message instance.
      *
@@ -25,7 +22,6 @@ class ThankYouForApplication extends Mailable
         $this->jobPost = $jobPost;
         $this->request = $request;
     }
-
     /**
      * Build the message.
      *
@@ -33,12 +29,24 @@ class ThankYouForApplication extends Mailable
      */
     public function build()
     {
-        return $this->from($this->jobPost->user->email, $this->jobPost->user->name)
-            ->subject('دریافت رزومه')
-            ->markdown('mails.exmpl')
-            ->with([
-                'name' => $this->request->name,
-                'link' => 'https://mailtrap.io/inboxes'
-            ]);
+        if(empty($this->jobPost->email_template)){
+            return $this->from('no-reply@bala-tar.com', $this->jobPost->user->name)
+                ->subject('دریافت رزومه')
+                ->markdown('mails.thankYouForApplication')
+                ->with([
+                    'company'=> $this->jobPost->company->name,
+                    'name' => $this->request->name,
+                ]);
+        }
+        if(!empty($this->jobPost->email_template)){
+            $content= str_replace(['{{name}}','{{company}}'], [$this->request->name,$this->jobPost->company->name], $this->jobPost->email_template);
+            return $this->from('no-reply@bala-tar.com', $this->jobPost->user->name)
+                ->subject('دریافت رزومه')
+                ->html($content)
+                ->with([
+                    'company'=> $this->jobPost->company->name,
+                    'name' => $this->request->name,
+                ]);
+        }
     }
 }
